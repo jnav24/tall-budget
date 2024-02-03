33da30e6b77eb1362fc8ecd0ee8a5d2e06cdfa66
Justin Navarro <info@justinnavarro.net>
2024-02-03 18:00:33 -0500

    update the dashboard home

--- resources/views/livewire/dashboard/home.blade.php
+++ resources/views/livewire/dashboard/home.blade.php
@@ -1,3 +1,5 @@
-<div>
-    Dashboard home
-</div>
+<main class="flex h-screen w-full items-center">
+    <section class="w-full rounded bg-gray-100 p-2 text-center text-gray-600 shadow">
+        Under construction
+    </section>
+</main>
