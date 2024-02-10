ee294d9702b029d78fcebc4077deb794d1e4a5c7
Justin Navarro <info@justinnavarro.net>
2024-02-10 10:42:22 -0500

    update the dashboard home page

--- resources/views/livewire/dashboard/home.blade.php
+++ resources/views/livewire/dashboard/home.blade.php
@@ -1,5 +1,7 @@
 <main class="flex h-screen w-full items-center">
-    <section class="w-full rounded bg-gray-100 p-2 text-center text-gray-600 shadow">
-        Under construction
+    <section class="w-full p-2 text-center">
+        <x-icons.truck class="mx-auto h-48 text-primary" />
+        <h1 class="text-2xl text-gray-600">Under Construction</h1>
+        <p class="text-gray-500">This page is still being worked on.</p>
     </section>
 </main>
