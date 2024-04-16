1e550901733bc95d574053b42aaaa0a07a27f189
Justin Navarro <info@justinnavarro.net>
2024-04-16 06:02:47 -0400

    created full name accessor

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -67,5 +68,12 @@ class UserProfile extends Model
     public function user(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    public function fullName(): Attribute
+    {
+        return Attribute::get(
+            fn () => "{$this->first_name} {$this->last_name}",
+        );
     }
 }
