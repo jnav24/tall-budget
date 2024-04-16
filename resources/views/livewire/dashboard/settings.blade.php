a96b810d0f2a531578bb8124ee676fa37ee416c3
Justin Navarro <info@justinnavarro.net>
2024-04-16 07:02:02 -0400

    created settings livewire component

---
+++ resources/views/livewire/dashboard/settings.blade.php
@@ -0,0 +1,11 @@
+<section class="mb-8 flex flex-row justify-between border-b-2 pb-8 align-top">
+    <div class="w-1/3">
+        <h3 class="font-header text-xl text-gray-800">gangstas</h3>
+        <p class="text-sm text-gray-600">make the...</p>
+    </div>
+    <div class="w-1/2">
+        <div class="bg-white my-2 p-4 rounded-md shadow-sm transition duration-150 sm:my-4">
+            world go round
+        </div>
+    </div>
+</section>
