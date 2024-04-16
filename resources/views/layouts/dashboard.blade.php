7c0b9113825d63866e1035aa34efd8f0757a88e7
Justin Navarro <info@justinnavarro.net>
2024-04-16 06:04:24 -0400

    added padding prop to dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.base')
+@props(['hasTopPadding' => true])
 
 @section('body')
     <livewire:dashboard.nav />
 
     <div class="h-screen overflow-y-auto bg-gray-200 pb-32">
-        <div class="container mx-auto" :class="{ 'pt-8': !noTopPadding }">
+        <div class="container mx-auto {{ $hasTopPadding ? 'pt-8' : '' }}">
             {{ $slot }}
         </div>
     </div>
