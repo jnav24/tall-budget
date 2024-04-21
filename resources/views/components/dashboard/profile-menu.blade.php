75d0ee139a093111059ec30db3b954228d0546b2
Justin Navarro <info@justinnavarro.net>
2024-04-20 21:34:01 -0400

    add watch to prevent buttons in profile dropdown from being clicked when drop down is not visible

--- resources/views/components/dashboard/profile-menu.blade.php
+++ resources/views/components/dashboard/profile-menu.blade.php
@@ -5,7 +5,26 @@
     ];
 @endphp
 
-<div class="relative" x-data="{ selected: false }" x-cloak>
+<div
+    class="relative"
+    x-data="{
+        selected: false,
+
+        init() {
+            $refs.subNav.classList.add('h-0', 'py-0');
+
+            $watch('selected', (value) => {
+                console.log(value);
+                if (!value) {
+                    setTimeout(() => $refs.subNav.classList.add('h-0', 'py-0'), 400);
+                } else {
+                    $refs.subNav.classList.remove('h-0', 'py-0');
+                }
+            })
+        },
+    }"
+    x-cloak
+>
     <button
         aria-label="Profile Menu"
         aria-haspopup="true"
@@ -38,6 +57,7 @@
             'translate-y-16 opacity-0': !selected,
             'translate-y-2 opacity-100': selected,
         }"
+        x-ref="subNav"
     >
         @foreach ($profile as $item)
             @if (! empty($item['to']))
