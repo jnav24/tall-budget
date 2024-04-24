f984894589026ae9f44144aa05ca7f23355c6a97
Justin Navarro <info@justinnavarro.net>
2024-04-24 06:28:00 -0400

    modal is working now

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -3,89 +3,102 @@
 @php
     $labelId = preg_replace('/\s+/', '_', strtolower($label));
     $hasError = $errors->has($labelId);
+    $hasItems = count($items) > 0;
 
     $styles = [
-        'border-solid border px-2 py-2 mt-2 rounded-md flex items-center justify-between outline-none transform relative',
-        $isDisabled ? 'bg-gray-200 border-gray-300 cursor-text text-gray-500' : '',
-        !$isDisabled && !$hasError ? 'border-gray-300 hover:border-gray-600 bg-white cursor-pointer text-gray-600 hover:text-gray-700 focus:border-primary transition duration-300' : '',
-        !$isDisabled && $hasError ? 'border-red-600 bg-white text-red-600' : '',
+        'border-solid border mt-2 rounded-md items-center justify-between outline-none transform relative',
+        $isDisabled || !$hasItems ? 'bg-gray-200 border-gray-300 cursor-text text-gray-500' : '',
+        !$isDisabled && !$hasError && $hasItems ? 'border-gray-300 hover:border-gray-600 bg-white cursor-pointer text-gray-600 hover:text-gray-700 focus:border-primary transition duration-300' : '',
+        !$isDisabled && $hasError && $hasItems ? 'border-red-600 bg-white text-red-600' : '',
     ];
 
     $styles = join(' ', $styles);
 @endphp
 
-<x-forms.label :label="$label" :label-id="$labelId" :has-error="$hasError" />
-
-<div
-    class="{{ $styles }}"
-    tabindex="0"
-    x-data="{
-        isDisabled: @js($isDisabled),
-        items: [],
-        itemLabel: @js($itemLabel),
-        itemValue: @js($itemValue),
-        selected: false,
-        selectedValue: '',
-
-        get placeHolder() {
-            return @js($placeholder);
-        },
-
-        init() {
-            $refs.dropDownItems.classList.add('h-0', 'py-0');
-            this.items = @js($items);
-        },
-
-        handleBlur() {
-            if (!this.isDisabled) {
-                this.selected = false;
-            }
-        },
-
-        handleClick() {
-            if (!this.isDisabled) {
-                this.selected = !this.selected;
-            }
-        },
-
-        setValue(value) {
-            this.selectedValue = value;
-            this.selected = false;
-        },
-
-        $watch('selected', (value) => {
-            if (value) {
-                setTimeout(() => {
-                    $refs.dropDownItems.classList.add('h-0', 'py-0');
-                }, 300);
-            } else {
-                $refs.dropDownItems.classList.remove('h-auto', 'py-1');
-            }
-        }),
-    }"
-    x-bind:class="{ 'z-50': selected, 'z-0': !selected }"
-    @blur="handleBlur()"
-    @click="handleClick()"
->
-    <input type="hidden" x-model="selectedValue">
-    <span class="flex-1" x-text="placeHolder"></span>
-    <span
-        class="transform transition duration-300"
-        x-bind:class="{ 'rotate-180': selected, 'rotate-0': !selected }"
-    >
-        <x-icons.chevron-down class="size-6" />
-    </span>
+<div>
+    <x-forms.label :label="$label" :label-id="$labelId" :has-error="$hasError" />
 
     <div
-        class="bg-white border border-gray-300 shadow-sm absolute transform top-0 left-0 rounded w-full transition ease-out duration-300 max-h-48 overflow-y-auto"
-        x-bind:class="{
-            'translate-y-12 opacity-100': selected,
-            'translate-y-0 opacity-0': !selected,
+        class="{{ $styles }}"
+        tabindex="0"
+        x-data="{
+            isDisabled: @js($isDisabled),
+            items: [],
+            itemLabel: @js($itemLabel),
+            itemValue: @js($itemValue),
+            selected: false,
+            selectedValue: '',
+
+            get placeHolder() {
+                const obj = this.items.find((item) => item[this.itemValue] === this.selectedValue);
+
+                if (this.selectedValue && obj) {
+                    return obj[this.itemLabel];
+                }
+
+                return @js($placeholder);
+            },
+
+            init() {
+                $refs.dropDownItems.classList.add('h-0', 'py-0');
+
+                $watch('selected', (value) => {
+                    if (!value) {
+                        setTimeout(() => {
+                            $refs.dropDownItems.classList.add('h-0', 'py-0');
+                        }, 300);
+                    } else {
+                        $refs.dropDownItems.classList.remove('h-0', 'py-0');
+                    }
+                });
+            },
+
+            handleBlur() {
+                if (!this.isDisabled) {
+                    this.selected = false;
+                }
+            },
+
+            handleClick() {
+                if (!this.isDisabled && this.items.length) {
+                    this.selected = !this.selected;
+                }
+            },
+
+            setValue(value) {
+                this.selectedValue = value;
+                this.selected = false;
+            },
         }"
-        ref="dropDownItems"
+        x-bind:class="{ 'z-50': selected, 'z-0': !selected }"
+        @blur="handleBlur()"
     >
-        <template x-for="(item, index) in items" :key="index">
-            <div class="hover:bg-gray-200 p-2" @click="setValue(item[itemValue])" x-text="item[itemValue]"></div>
-        </template>
+        <input type="hidden" x-model="selectedValue">
+
+        <div
+            class="w-full flex px-2 py-2 items-center"
+            @click="handleClick()"
+        >
+            <span class="flex-1" x-text="placeHolder"></span>
+            <span
+                class="transform transition duration-300"
+                x-bind:class="{ 'rotate-180': selected, 'rotate-0': !selected }"
+            >
+                <x-icons.chevron-down class="size-4" />
+            </span>
+        </div>
+
+        <div
+            class="bg-white border border-gray-300 shadow-sm absolute transform top-0 left-0 rounded w-full transition ease-out duration-300 max-h-48 overflow-y-auto"
+            x-bind:class="{
+                'translate-y-12 opacity-100': selected,
+                'translate-y-0 opacity-0': !selected,
+            }"
+            x-ref="dropDownItems"
+        >
+            <template x-for="(item, index) in items" :key="index">
+                <div class="hover:bg-gray-200 p-2" @click="setValue(item[itemValue])" x-text="item[itemLabel]"></div>
+            </template>
+        </div>
     </div>
 </div>
