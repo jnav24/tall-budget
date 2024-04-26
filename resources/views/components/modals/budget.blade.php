d502c3511654c465420cb226eeeaceddc01fe1f5
Justin Navarro <info@justinnavarro.net>
2024-04-26 15:44:16 -0400

    dispatching select component events to the parent in the budget modal

--- resources/views/components/modals/budget.blade.php
+++ resources/views/components/modals/budget.blade.php
@@ -1,18 +1,41 @@
-<div class="w-100" x-data="{ isSubmit: false }">
+@props(['budget-months'])
+
+<div
+    class="w-100"
+    x-data="{
+        form: {},
+        isSubmit: false,
+
+        updateForm(event) {
+            const { detail } = event;
+            this.form = { ...this.form, ...detail };
+            console.log(detail);
+            console.log(this.form);
+        },
+
+        submitForm() {
+            this.isSubmit = true;
+            $dispatch('add-new-budget', this.form);
+            // @todo handle routing
+            this.isSubmit = false;
+        },
+    }"
+>
     <h2
         class="text-2xl text-gray-700 font-body mb-4 px-4 pt-2 absolute lg:relative top-0 left-0 w-full bg-white lg:bg-opacity-0"
     >
         Monthly Budget
     </h2>
 
-    <div class="px-4 mb-4 grid grid-cols-2 gap-3">
-        Content here
+    <div class="px-4 mb-4 grid grid-cols-2 gap-3" @handle-on-change.window="updateForm($event)">
+        <x-forms.select label="Month" :items="$budgetMonths" />
+        <x-forms.select label="Year" :items="[]" />
     </div>
 
     <div
         class="bg-gray-100 rounded-b-lg p-4 flex flex-row items-end justify-end space-x-2"
     >
         <x-forms.button @click="$dispatch('close-modal')">Cancel</x-forms.button>
-        <x-forms.button color="secondary" x-bind:disabled="isSubmit" @click="isSubmit = true; $dispatch('add-new-budget');">New Budget</x-forms.button>
+        <x-forms.button color="secondary" x-bind:disabled="isSubmit" @click="submitForm()">New Budget</x-forms.button>
     </div>
 </div>
