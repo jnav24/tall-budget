690d1c1dd842c527572c9b481ba47597257f48de
Justin Navarro <info@justinnavarro.net>
2024-04-26 19:04:00 -0400

    update the sidebaritem based on changes in the budget component

--- resources/views/components/dashboard/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar.blade.php
@@ -4,19 +4,19 @@
     <p class="mb-6 text-lg text-gray-700 font-body">{{ $title }}</p>
 
     <ul>
-        <template x-for="(label, i) in sidebarItems">
+        <template x-for="(item, i) in sidebarItems">
             <li
                 :key="i"
                 class="flex flex-row items-center justify-between"
                 :class="{
 					'text-gray-500 text-sm hover:text-gray-700 hover:bg-gray-100 cursor-pointer px-2 py-2':
-						label !== selectedSidebar,
+						item.value !== selectedSidebar,
 					'text-gray-700 text-sm cursor-pointer px-2 py-2 bg-gray-300 rounded':
-						label === selectedSidebar,
+						item.value === selectedSidebar,
 				}"
-                @click="selectedSidebar = label"
+                @click="selectedSidebar = item.value"
             >
-                <span x-text="label"></span>
+                <span x-text="item.label"></span>
                 <span class="bg-danger text-white text-center rounded-full h-6 min-w-6 flex items-center justify-center">1</span>
             </li>
         </template>
