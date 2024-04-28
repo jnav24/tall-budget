41693b076fa91f03405966ce0ac3031e5848ab6b
Justin Navarro <info@justinnavarro.net>
2024-04-28 09:28:22 -0400

    add custom padding to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,53 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     theme: {
         extend: {
             colors: {
-                danger: "#C62828",
-                "dark-danger": "#4B0F0F",
-                "dark-primary": "#264653",
-                "dark-secondary": "#A88D3E",
-                primary: "#45ADA8",
-                secondary: "#EFC958",
-                success: "#198754",
+                danger: '#C62828',
+                'dark-danger': '#4B0F0F',
+                'dark-primary': '#264653',
+                'dark-secondary': '#A88D3E',
+                primary: '#45ADA8',
+                secondary: '#EFC958',
+                success: '#198754',
             },
             fontFamily: {
-                body: ["Nunito", "sans-serif"],
-                header: ["Avenir", "Helvetica", "Arial", "sans-serif"],
-                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+                body: ['Nunito', 'sans-serif'],
+                header: ['Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
             minHeight: {
-                64: "16rem",
-                256: "64rem",
+                64: '16rem',
+                256: '64rem',
             },
             maxHeight: {
-                48: "12rem",
+                48: '12rem',
             },
             maxWidth: {
-                32: "8rem",
+                32: '8rem',
             },
             minWidth: {
-                6: "1.5rem",
+                6: '1.5rem',
             },
             opacity: {
-                85: "0.85",
-                95: "0.95",
+                85: '0.85',
+                95: '0.95',
+            },
+            padding: {
+                30: '7.5rem',
             },
             translate: {
-                "6/7": "85.7142857%",
-                "7/8": "87.5%",
-                "9/10": "90%",
+                '6/7': '85.7142857%',
+                '7/8': '87.5%',
+                '9/10': '90%',
             },
             width: {
-                100: "25rem",
-                150: "38rem",
-                200: "50rem",
-                250: "63rem",
-                300: "75rem",
+                100: '25rem',
+                150: '38rem',
+                200: '50rem',
+                250: '63rem',
+                300: '75rem',
             },
             zIndex: {
                 100: 100,
@@ -53,22 +56,19 @@ module.exports = {
     },
     variants: {
         extend: {
-            backgroundColor: ["active"],
+            backgroundColor: ['active'],
         },
     },
     content: [
-        "./app/**/*.php",
-        "./resources/**/*.html",
-        "./resources/**/*.js",
-        "./resources/**/*.jsx",
-        "./resources/**/*.ts",
-        "./resources/**/*.tsx",
-        "./resources/**/*.php",
-        "./resources/**/*.vue",
-        "./resources/**/*.twig",
+        './app/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+        './resources/**/*.php',
+        './resources/**/*.vue',
+        './resources/**/*.twig',
     ],
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography"),
-    ],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
