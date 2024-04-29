dde2c5c182d948557dc6124c9ba844c15379b5ac
Justin Navarro <info@justinnavarro.net>
2024-04-29 08:09:45 -0400

    add childcare models

---
+++ app/Models/Childcare.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Childcare extends Model
+{
+    use HasFactory;
+
+    /**
+     * Default Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name' => null,
+        'amount' => null,
+        'due_date' => null,
+        'paid_date' => null,
+        'confirmation' => null,
+        'not_track_amount' => null,
+        'childcare_type_id' => null,
+        'budget_id' => null,
+        'notes' => null,
+    ];
+
+    /**
+     * Mass assignment
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'amount',
+        'due_date',
+        'paid_date',
+        'confirmation',
+        'not_track_amount',
+        'childcare_type_id',
+        'budget_id',
+        'notes',
+    ];
+
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'childcare';
+
+    /**
+     * @return HasOne
+     */
+    public function type()
+    {
+        return $this->hasOne(ChildcareType::class, 'id', 'bank_type_id');
+    }
+}
