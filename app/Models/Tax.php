45d12c332b9a127907adcc168dd47b638f2dfb28
Justin Navarro <info@justinnavarro.net>
2024-04-30 04:41:56 -0400

    add tax models

---
+++ app/Models/Tax.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Tax extends Model
+{
+    use HasFactory;
+
+    /**
+     * Default Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name' => null,
+        'amount' => null,
+        'due_date' => null,
+        'paid_date' => null,
+        'confirmation' => null,
+        'not_track_amount' => null,
+        'tax_type_id' => null,
+        'budget_id' => null,
+        'notes' => null,
+    ];
+
+    /**
+     * Mass assignment
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'amount',
+        'due_date',
+        'paid_date',
+        'confirmation',
+        'not_track_amount',
+        'tax_type_id',
+        'budget_id',
+        'notes',
+    ];
+
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'tax';
+
+    /**
+     * @return HasOne
+     */
+    public function type(): HasOne
+    {
+        return $this->hasOne(TaxType::class, 'id', 'tax_type_id');
+    }
+}
