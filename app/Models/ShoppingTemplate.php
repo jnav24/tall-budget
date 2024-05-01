a4e3799b8fae48a7b75f472b711dbbd7083654eb
Justin Navarro <info@justinnavarro.net>
2024-05-01 06:13:41 -0400

    add shopping models

---
+++ app/Models/ShoppingTemplate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShoppingTemplate extends Model
+{
+    use HasFactory;
+    /**
+     * Default Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name' => null,
+        'amount' => null,
+        'due_date' => null,
+        'shopping_type_id' => null,
+        'budget_template_id' => null,
+    ];
+
+    /**
+     * Mass assignment
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'amount',
+        'due_date',
+        'shopping_type_id',
+        'budget_template_id',
+    ];
+
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_template_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'shopping_templates';
+}
