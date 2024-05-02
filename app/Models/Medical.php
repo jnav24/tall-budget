9ecb8cd5c784842bfcc810f31d6b872cddfd73c1
Justin Navarro <info@justinnavarro.net>
2024-05-02 05:49:41 -0400

    add medical models

---
+++ app/Models/Medical.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Medical extends Model
+{
+    use HasFactory;
+
+    /**
+     * Default Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name' => null,
+        'amount' => null,
+        'due_date' => null,
+        'medical_type_id' => null,
+        'paid_date' => null,
+        'confirmation' => null,
+        'not_track_amount' => null,
+        'budget_id' => null,
+        'notes' => null,
+    ];
+
+    /**
+     * Mass assignment
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'amount',
+        'due_date',
+        'medical_type_id',
+        'paid_date',
+        'confirmation',
+        'not_track_amount',
+        'budget_id',
+        'notes',
+    ];
+
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'medical';
+
+    /**
+     * @return HasOne
+     */
+    public function type(): HasOne
+    {
+        return $this->hasOne(MedicalType::class, 'id', 'medical_type_id');
+    }
+}
