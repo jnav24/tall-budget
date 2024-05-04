a7a88cc506dbd34ff5faeccf328af6f4c4ae0b3c
Justin Navarro <info@justinnavarro.net>
2024-05-04 12:25:54 -0400

    add income models

---
+++ app/Models/Income.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Income extends Model
+{
+    use HasFactory;
+
+    /**
+     * Default Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'name' => null,
+        'amount' => null,
+        'income_type_id' => null,
+        'initial_pay_date' => null,
+        'budget_id' => null,
+    ];
+
+    /**
+     * Mass assignment
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'amount',
+        'income_type_id',
+        'initial_pay_date',
+        'budget_id',
+    ];
+
+    /**
+     * Hide columns
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'budget_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    protected $table = 'incomes';
+
+    /**
+     * @return HasOne
+     */
+    public function type(): HasOne
+    {
+        return $this->hasOne(IncomeType::class, 'id', 'income_type_id');
+    }
+}
