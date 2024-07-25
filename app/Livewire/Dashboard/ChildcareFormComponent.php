1dbfcd20f37b5532421f2180a5446bdcd49a67ac
Justin Navarro <info@justinnavarro.net>
2024-07-25 05:05:19 -0400

    setup the childcare form component

---
+++ app/Livewire/Dashboard/ChildcareFormComponent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Livewire\Component;
+
+class ChildcareFormComponent extends Component
+{
+    /** @var array<string>  */
+    public array $types;
+
+    public function mount($types = []): void
+    {
+        $this->types = array_map(fn ($type) => ['label' => $type['name'], 'value' => $type['id']], $types);
+    }
+
+    public function save(): void
+    {
+        $this->validate();
+    }
+
+    public function render(): View|Application|Factory
+    {
+        return view('livewire.dashboard.childcare-form-component');
+    }
+}
---
+++ resources/views/livewire/dashboard/childcare-form-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Childcare form
+</div>
