efdb025b495424407ec925429ecdbe2a0f3349bf
Justin Navarro <info@justinnavarro.net>
2024-07-26 16:05:45 -0400

    update the checkbox component to make the actual check invisible when it is not checked

--- resources/views/components/forms/checkbox.blade.php
+++ resources/views/components/forms/checkbox.blade.php
@@ -13,7 +13,7 @@
 <div class="">
     <div class="flex flex-row items-center space-x-2">
         <x-forms.button checkbox {{ $attributes }} color="{{ $checked ? 'primary' : 'default' }}">
-            <x-icons.check class="size-4 {{ !$checked ? 'text-white' : '' }}" />
+            <x-icons.check class="size-4 text-white {{ !$checked ? 'invisible' : '' }}" />
         </x-forms.button>
         <x-forms.label :label="$label" :label-id="$labelId" :has-error="$hasError" />
     </div>
