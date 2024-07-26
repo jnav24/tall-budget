1343d78e1ef6c748ea2eae5dfaf8164cf8848f70
Justin Navarro <info@justinnavarro.net>
2024-07-26 16:06:45 -0400

    created the common expense form class

---
+++ app/Livewire/Forms/CommonExpenseForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+
+class CommonExpenseForm extends Form
+{
+    #[Validate('required')]
+    public string $name;
+
+    #[Validate('required|decimal:2')]
+    public string $amount;
+
+    #[Validate('required')]
+    public string $type;
+
+    #[Validate('string')]
+    public string $confirmation = '';
+
+    #[Validate('bool')]
+    public bool $calculate = false;
+
+    #[Validate('required|date')]
+    public string $paidDate;
+
+    #[Validate('string')]
+    public string $notes = '';
+
+    public function updateCalculate(): void
+    {
+        $this->calculate = ! $this->calculate;
+    }
+}
